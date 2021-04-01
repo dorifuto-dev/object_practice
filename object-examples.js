62ddb80bbae4431d265c4e52756b8e7184222e77
Eric Li <ericli@knights.ucf.edu>
2021-04-01 13:52:01 -0600

    Add animal to object-examples.js

--- object-examples.js
+++ object-examples.js
@@ -1,7 +1,15 @@
-var car ={
+var car = {
   make: "Honda",
   model: "S2000",
   year: "2002",
   color: "Sebring Silver",
   engine: "F20C"
+}
+
+var animal = {
+  commonName: "Dog",
+  genus: "canis",
+  species: "lupus",
+  subspecies: "familiaris",
+  name: "Cloud"
 }
